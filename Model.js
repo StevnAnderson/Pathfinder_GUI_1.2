3de72ab526d551926f47bc795931332ef57c8a50
alex a <swagger143@gmail.com>
2017-03-26 00:33:13 -0600

    added animal companion function

--- Model.js
+++ Model.js
@@ -1,3 +1,19 @@
 function diceRoll() {
 
 }
+
+function companionGenerator(){
+	this.name
+	
+	this.str;
+	this.dex;
+	this.con;
+	this.int;
+	this.wis;
+	this.cha;
+	
+	this.create(){
+		
+	}
+	
+}
